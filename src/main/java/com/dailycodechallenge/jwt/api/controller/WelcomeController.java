bf5aa890cb368cdd7043dc8508ed5b9bfb45f656
Sanoop <sanoop.k@teksalah.com>
2022-12-06 12:39:50 +0530

    JWT initial commit

---
+++ src/main/java/com/dailycodechallenge/jwt/api/controller/WelcomeController.java
@@ -0,0 +1,37 @@
+package com.dailycodechallenge.jwt.api.controller;
+
+import com.dailycodechallenge.jwt.api.util.JwtUtil;
+import com.dailycodechallenge.jwt.api.entity.AuthRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @GetMapping("/")
+    public String welcome() {
+        return "Login Success! !";
+    }
+
+    @PostMapping("/authenticate")
+    public String generateToken(@RequestBody AuthRequest authRequest) throws Exception {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authRequest.getUserName(), authRequest.getPassword())
+            );
+        } catch (Exception ex) {
+            throw new Exception("inavalid username/password");
+        }
+        return jwtUtil.generateToken(authRequest.getUserName());
+    }
+}
---
+++ src/main/java/com/dailycodechallenge/jwt/api/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.dailycodechallenge.jwt.api.repository;
+
+import com.dailycodechallenge.jwt.api.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+    User findByUserName(String username);
+}
